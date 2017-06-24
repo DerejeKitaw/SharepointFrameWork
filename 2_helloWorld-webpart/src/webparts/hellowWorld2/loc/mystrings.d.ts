59b667795c40a04da3595d67f186339fa9381b5a
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-06-23 23:55:49 -0400

    webpart modified

--- 2_helloWorld-webpart/src/webparts/hellowWorld2/loc/mystrings.d.ts
+++ 2_helloWorld-webpart/src/webparts/hellowWorld2/loc/mystrings.d.ts
@@ -2,6 +2,7 @@ declare interface IHellowWorld2Strings {
   PropertyPaneDescription: string;
   BasicGroupName: string;
   DescriptionFieldLabel: string;
+  UserNameFieldLable:string;
 }
 
 declare module 'hellowWorld2Strings' {
