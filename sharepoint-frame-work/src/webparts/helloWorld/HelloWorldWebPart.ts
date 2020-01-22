1352f7b67c7bcd4be174cda9101195553c54c3b1
Dereje Kitaw <dereje.kitaw@gmail.com>
2020-01-22 02:16:04 -0500

    Connect client-side web part to SharePoint

--- sharepoint-frame-work/src/webparts/helloWorld/HelloWorldWebPart.ts
+++ sharepoint-frame-work/src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -1,4 +1,5 @@
 import { Version } from "@microsoft/sp-core-library";
+import { Environment, EnvironmentType } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField,
@@ -11,36 +12,94 @@ import { escape } from "@microsoft/sp-lodash-subset";
 
 import styles from "./HelloWorldWebPart.module.scss";
 import * as strings from "HelloWorldWebPartStrings";
-
+import MockHttpClient from "./MockHttpClient";
+import { SPHttpClient, SPHttpClientResponse } from "@microsoft/sp-http";
 export interface IHelloWorldWebPartProps {
   description: string;
-  test:string;
-  test1:string;
-  test2:string;
-  test3:boolean;
+  test: string;
+  test1: string;
+  test2: string;
+  test3: boolean;
 }
-
+export interface ISPLists {
+  value: ISPList[];
+}
+export interface ISPList {
+  Title: string;
+  Id: string;
+}
 export default class HelloWorldWebPart extends BaseClientSideWebPart<
   IHelloWorldWebPartProps
 > {
+  private _getMockListData(): Promise<ISPLists> {
+    return MockHttpClient.get().then((data: ISPList[]) => {
+      var listData: ISPLists = { value: data };
+      return listData;
+    }) as Promise<ISPLists>;
+  }
+  private _getListData(): Promise<ISPLists> {
+    return this.context.spHttpClient
+      .get(
+        this.context.pageContext.web.absoluteUrl +
+          `/_api/web/lists?$filter=Hidden eq false`,
+        SPHttpClient.configurations.v1
+      )
+      .then((response: SPHttpClientResponse) => {
+        return response.json();
+      });
+  }
+  private _renderList(items: ISPList[]): void {
+    let html: string = "";
+    items.forEach((item: ISPList) => {
+      html += `
+    <ul class="${styles.list}">
+      <li class="${styles.listItem}">
+        <span class="ms-font-l">${item.Title}</span>
+      </li>
+    </ul>`;
+    });
+
+    const listContainer: Element = this.domElement.querySelector(
+      "#spListContainer"
+    );
+    listContainer.innerHTML = html;
+  }
+  private _renderListAsync(): void {
+    // Local environment
+    if (Environment.type === EnvironmentType.Local) {
+      this._getMockListData().then((response) => {
+        this._renderList(response.value);
+      });
+    }
+    else if (Environment.type == EnvironmentType.SharePoint ||
+              Environment.type == EnvironmentType.ClassicSharePoint) {
+      this._getListData()
+        .then((response) => {
+          this._renderList(response.value);
+        });
+    }
+  }
   public render(): void {
     this.domElement.innerHTML = `
-      <div class="${styles.helloWorld}">
-    <div class="${styles.container}">
-      <div class="${styles.row}">
-        <div class="${styles.column}">
-          <span class="${styles.title}">Welcome to SharePoint!</span>
-  <p class="${
-    styles.subTitle
-  }">Customize SharePoint experiences using Web Parts.</p>
-    <p class="${styles.description}">${escape(this.properties.description)}</p>
-      <a href="https://aka.ms/spfx" class="${styles.button}">
-        <span class="${styles.label}">Learn more</span>
+  <div class="${ styles.helloWorld }">
+    <div class="${ styles.container }">
+      <div class="${ styles.row }">
+        <div class="${ styles.column }">
+          <span class="${ styles.title }">Welcome to SharePoint!</span>
+          <p class="${ styles.subTitle }">Customize SharePoint experiences using web parts.</p>
+          <p class="${ styles.description }">${escape(this.properties.description)}</p>
+          <p class="${ styles.description }">${escape(this.properties.test)}</p>
+          <p class="${ styles.description }">Loading from ${escape(this.context.pageContext.web.title)}</p>
+          <a href="https://aka.ms/spfx" class="${ styles.button }">
+            <span class="${ styles.label }">Learn more</span>
           </a>
-          </div>
-          </div>
-          </div>
-          </div>`;
+        </div>
+      </div>
+      <div id="spListContainer" />
+    </div>
+  </div>`;
+
+this._renderListAsync();
   }
 
   protected get dataVersion(): Version {
